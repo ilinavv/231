eaa83250318c58d5d34d1254d3cd8ffe0c3a23e8
ilinavv <vika.mimm3@gmail.com>
2025-01-16 03:04:51 +0000

    Thu, Jan 16, 2025, 10:04 AM +07:00

--- flights/views.py
+++ flights/views.py
@@ -7,3 +7,5 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.object.get(id=flight_id)
+    return render(request, "flights/flight.html",
+                  {"flight": flight})
