6e11de1bb199155dde0d38298564f38b8074ed40
ilinavv <vika.mimm3@gmail.com>
2025-01-16 03:44:20 +0000

    Thu, Jan 16, 2025, 10:44 AM +07:00

--- flights/models.py
+++ flights/models.py
@@ -13,3 +13,8 @@ class Flight(models.Model):
     duration = models.IntegerField()
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
