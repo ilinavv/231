7484cb176df38bf2a11c93fb235f468a776001a3
ilinavv <vika.mimm3@gmail.com>
2025-01-16 03:03:04 +0000

    Thu, Jan 16, 2025, 10:03 AM +07:00

--- flights/views.py
+++ flights/views.py
@@ -4,3 +4,6 @@ from .models import Flight
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {"flights": Flight.objects.all()})
+
+def flight(request, flight_id):
+    flight = Flight.object.get(id=flight_id)
