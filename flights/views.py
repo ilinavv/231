c5d70bf1c5979e940ee583d093bc3c01b2001687
ilinavv <vika.mimm3@gmail.com>
2025-01-16 04:24:19 +0000

    Thu, Jan 16, 2025, 11:24 AM +07:00

--- flights/views.py
+++ flights/views.py
@@ -12,4 +12,11 @@ def flight(request, flight_id):
                   {"flight": flight,
                    "passengers": passengers})
 
-def book(request, flight_id)
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
