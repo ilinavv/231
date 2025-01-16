76bb913eba2b33073ea37c38e9909d0b9ae4140d
ilinavv <vika.mimm3@gmail.com>
2025-01-16 04:27:23 +0000

    Thu, Jan 16, 2025, 11:27 AM +07:00

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Flight
 from django.http import HttpResponseRedirect
-from
+from django.urls import reverse
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {"flights": Flight.objects.all()})
